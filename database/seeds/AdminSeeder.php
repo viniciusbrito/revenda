b0252668e3a4b6ebfa3c6c9ef5a425421e98d62c
Vinicius <vinicius.fernandes.brito@gmail.com>
2017-03-29 23:58:05 -0300

    Created Admin and User seed

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $adm = [
+            'name' => 'Vinícius Brito',
+            'email' => 'vinicius.fernandes.brito@gmail.com',
+            'password' => bcrypt('senha123'),
+            'remember_token' => str_random(10),
+        ];
+        DB::table('admins')->create($adm);
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $usr = [
+            'name' => 'Vinícius Brito',
+            'email' => 'vinicius.fernandes.brito@gmail.com',
+            'cpf' => '022.181.461-26',
+            'password' => bcrypt('senha123'),
+            'remember_token' => str_random(10),
+        ];
+        DB::table('users')->create($usr);
+    }
+}
