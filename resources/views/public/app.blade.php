470b4d4581347ab8b80f12ac697417834d363313
viniciusbrito <vinicius.fernandes.brito@gmail.com>
2017-07-12 18:11:44 -0300

    Landpage prototype

---
+++ resources/views/public/app.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="https://dottcon.com/_biblioteca/_imagens/favicon.ico" type="image/x-icon"/>
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+</head>
+    <div id="app">
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a href="{{ url('/') }}">
+                        <img class="navbar-brand" src="https://dottcon.com/_biblioteca/_imagens/imgLogoEC-2.png" alt="Dottcon - Tecnologia & Consultoria"/>
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <a class="btn btn-sm btn-success navbar-btn" href="{{ route('register') }}" t><i class="glyphicon glyphicon-plus"></i> <b>Cadastre-se</b></a>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#domains">Domínios</a></li>
+                        <li><a href="#plans">Hospedagem</a></li>
+                        <li><a href="#contact">Contato</a></li>
+                    </ul>
+                </div>
+            </div>
+            {{--GAMBIARRA PARA CORRIGIR--}}
+            <div class="progress" style="margin-bottom: 0; height: 2px;">
+                <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">
+                    <span class="sr-only">100% Complete</span>
+                </div>
+            </div>
+            {{--GAMBIARRA PARA CORRIGIR--}}
+        </nav>
+
+        @yield('content')
+        <div id="contact" style=" background-color: #fff; margin-top: 0; padding-top:1%; height:80px; box-shadow: 0 -5px 10px #5cb85c;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <img class="img-responsive" src="https://dottcon.com/_biblioteca/_imagens/imgLogoEC-2.png" alt="Dottcon - Tecnologia & Consultoria"/>
+                        <small>&copy; {{date('Y')}}</small>
+                    </div>
+
+                    <div class="col-sm-2">
+                        <small style="font-weight:bold; color: #000;">
+                            Rua Goiás, 421 - Centro <br/>
+                            CEP 78600-000 <br/>
+                            Barra do Garças, MT - Brasil
+                        </small>
+                    </div>
+
+                    <div class="col-sm-2">
+                        <small style="font-weight:bold; color: #000;">(66) 98463-9404<br/>contato&commat;dottcon.com.br</small>
+                    </div>
+
+                    <div class="col-sm-1 text-right">
+                        <a href="https://fb.com/dottcon"><i style="color: #3b5998" class="fa fa-facebook-official fa-2x" aria-hidden="true"></i></a>
+                        &nbsp;
+                        <a href="https://plus.google.com/+dottcon"><i style="color: #ea4335" class="fa fa-google-plus-official fa-2x" aria-hidden="true"></i></a>
+                    </div>
+
+                    <div class="col-sm-5">
+                        <img class="img-responsive" src="https://stc.pagseguro.uol.com.br/public/img/banners/pagamento/avista_estatico_550_70.gif" alt="Logotipos de meios de pagamento do PagSeguro" title="Este site aceita pagamentos com os principais bancos, saldo em conta PagSeguro e boleto.">
+                    </div>
+                </div>
+                <div class="row">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -1,25 +1,115 @@
-@extends('layouts.app')
+@extends('public.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <div class="row">
-                            @foreach(Revenda\CPanel\Pacote::all() as $pacote)
-                                <div class="col-sm-4 col-xs-12">
-                                    <div class="jumbotron">
-                                        <strong>{{$pacote->nome}}</strong><br/>
-                                        <strong>Valor: R$ {{$pacote->valor}}</strong><br/>
-                                        <a href="{{route('client.account.create')}}" class="btn btn-sm btn-primary">Comprar</a>
-                                    </div>
+
+    <div id="domains" style="background-color:#f8f8f8; margin-top: 50px; padding-top:2%; height:130px;">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-10 col-sm-offset-1">
+                    <h5>Encontre o seu novo nome de domínio. Digite seu nome ou palavras-chave abaixo para verificar a disponibilidade.</h5>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="input-group input-group-lg">
+                        <input id="input-search-domain" type="text" class="form-control" placeholder="Encontre seu novo nome de domínio ...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-lg btn-success" type="button"><i class="glyphicon glyphicon-search"></i> Pesquisar domínio</button>
+                            </span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="plans" style="background-color:#5cb85c; margin-top: 0; padding-top:5%; height:500px; box-shadow: 0px -2px 5px #5cb85c;">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3 style="color: #fff">HOSPEDAGEM DE SITES</h3>
+                            <h1 style="color: #fff">ESCOLHA JÁ O SEU PLANO</h1>
+                        </div>
+                    </div>
+
+                    <div class="row" style="margin-top: 5%;">
+                        <div class="col-sm-3 col-sm-offset-2">
+                            <div class="panel">
+                                <div class="panel-body text-center" style="box-shadow: 1px 1px 10px;">
+                                    <h3>PLANO<br/>BÁSICO</h3>
+                                    <H4>R$ 9.90</H4>
+                                    <button class="btn btn-lg btn-block btn-warning"><i class="glyphicon glyphicon-shopping-cart"></i> Contratar</button>
+                                    <br/>
+                                    <p>10 GB Espaço em Disco</p>
+                                    <p>1 Domínio Hospedado</p>
+                                    <p>Transferência ILIMITADA</p>
+                                    <p>Contas de E-Mail ILIMITADAS</p>
+                                    <p>Bases MySQL ILIMITADAS</p>
+                                    <p>Subdomínios ILIMITADOS</p>
+                                    <p>Certificado SSL GRÁTIS</p>
+                                    <p>Criador de Sites GRÁTIS</p>
+                                    <p>Instalador +430 Apps GRÁTIS</p>
+                                    <p>CPU Xeon 1 x 2,4GHz</p>
+                                    <p>Memória 10GB (2 Ram + 8 Virt)</p>
+                                    <p>Processos Simultâneos 50</p>
+                                    <p>Inodes ILIMITADOS</p>
                                 </div>
-                            @endforeach
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="panel">
+                                <div class="panel-body text-center" style="box-shadow: 1px 1px 10px;">
+                                    <h3>PLANO<br/>INTERMEDIÁRIO</h3>
+                                    <H4>R$ 19.90</H4>
+                                    <button class="btn btn-lg btn-block btn-warning"><i class="glyphicon glyphicon-shopping-cart"></i> Contratar</button>
+                                    <br/>
+                                    <p>10 GB Espaço em Disco</p>
+                                    <p>1 Domínio Hospedado</p>
+                                    <p>Transferência ILIMITADA</p>
+                                    <p>Contas de E-Mail ILIMITADAS</p>
+                                    <p>Bases MySQL ILIMITADAS</p>
+                                    <p>Subdomínios ILIMITADOS</p>
+                                    <p>Certificado SSL GRÁTIS</p>
+                                    <p>Criador de Sites GRÁTIS</p>
+                                    <p>Instalador +430 Apps GRÁTIS</p>
+                                    <p>CPU Xeon 1 x 2,4GHz</p>
+                                    <p>Memória 10GB (2 Ram + 8 Virt)</p>
+                                    <p>Processos Simultâneos 50</p>
+                                    <p>Inodes ILIMITADOS</p>
+                                </div>
+                              </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="panel">
+                                <div class="panel-body text-center" style="box-shadow: 1px 1px 10px;">
+                                    <h3>PLANO<br/>AVANÇADO</h3>
+                                    <H4>R$ 39.90</H4>
+                                    <button class="btn btn-lg btn-block btn-warning"><i class="glyphicon glyphicon-shopping-cart"></i> Contratar</button>
+                                    <br/>
+                                    <p>10 GB Espaço em Disco</p>
+                                    <p>1 Domínio Hospedado</p>
+                                    <p>Transferência ILIMITADA</p>
+                                    <p>Contas de E-Mail ILIMITADAS</p>
+                                    <p>Bases MySQL ILIMITADAS</p>
+                                    <p>Subdomínios ILIMITADOS</p>
+                                    <p>Certificado SSL GRÁTIS</p>
+                                    <p>Criador de Sites GRÁTIS</p>
+                                    <p>Instalador +430 Apps GRÁTIS</p>
+                                    <p>CPU Xeon 1 x 2,4GHz</p>
+                                    <p>Memória 10GB (2 Ram + 8 Virt)</p>
+                                    <p>Processos Simultâneos 50</p>
+                                    <p>Inodes ILIMITADOS</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <div id="" style="background-color:#f8f8f8; margin-top: 0; height:380px;"></div>
 @endsection
